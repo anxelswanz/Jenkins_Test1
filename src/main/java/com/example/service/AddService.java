08f3516d91a25e6c04e7538423139f43147eab5b
anxelswanz <122070873+anxelswanz@users.noreply.github.com>
2024-09-14 07:14:02 +0800

    5

--- src/main/java/com/example/service/AddService.java
+++ src/main/java/com/example/service/AddService.java
@@ -1,11 +1,14 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author Ansel Zhong
  * @description:
  * @date 2024/9/14 7:08
  * @ProjectName Jenkins_Test1
  **/
+@Service
 public class AddService {
     public Result add(int a, int b) {
         int sum = a / 0;
